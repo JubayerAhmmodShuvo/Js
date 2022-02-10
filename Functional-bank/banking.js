a03b552918d4e32330a491036966bf2c8d7d8b29
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-11 00:00:43 +0600

    practicing

--- Functional-bank/banking.js
+++ Functional-bank/banking.js
@@ -1,7 +1,19 @@
-document.getElementById('deposit-button').addEventListener('click', function() {
+function getInputValue() {
     const depositInput = document.getElementById('deposit-input');
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText);
+
+    //clear input field
+    depositInput.value = '';
+    return depositAmount;
+}
+
+document.getElementById('deposit-button').addEventListener('click', function() {
+    /*  const depositInput = document.getElementById('deposit-input');
+     const depositAmountText = depositInput.value;
+     const depositAmount = parseFloat(depositAmountText); */
+
+    const depositAmount = getInputValue();
 
     //get current deposit5
     const depositTotal = document.getElementById('deposit-total');
@@ -15,5 +27,26 @@ document.getElementById('deposit-button').addEventListener('click', function() {
     balanceTotal.innerText = previousBalanceText + depositAmount;
     //clear input field
     depositInput.value = '';
+
+});
+
+//withdraw
+document.getElementById('withdraw-button').addEventListener('click', function() {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    //withdraw deposit amount
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawText = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawText + withdrawAmount;
+    //clear field
+    withdrawInput.value = '';
+    //update balance input
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceText = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceText - withdrawAmount;
 
 });
