9ab9f991576fa8529d26b5bce892b602ec6773d2
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-12 23:55:55 +0600

    dooing

--- shopping-cart-solution/app.js
+++ shopping-cart-solution/app.js
@@ -1,5 +1,28 @@
-document.getElementById('case-plus').addEventListener('click', function() {
+function updateCaseNumber(isIncreasing) {
     const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    } else if (
+        caseNumber > 0) {
+        caseNumber = parseInt(
+            caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    //update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function() {
+    /* const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-})
+    caseInput.value = parseInt(caseNumber) + 1; */
+    updateCaseNumber(true);
+});
+document.getElementById('case-minus').addEventListener('click', function() {
+    /*  const caseInput = document.getElementById('case-number');
+     const caseNumber = caseInput.value; */
+    updateCaseNumber(false);
+
+});
