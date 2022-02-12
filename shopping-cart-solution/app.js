7915982cc265615e2fffe281f516c69c74739bc1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-13 00:43:35 +0600

    going

--- shopping-cart-solution/app.js
+++ shopping-cart-solution/app.js
@@ -12,28 +12,46 @@ function updateProductNumber(product, price, isIncreasing) {
     //update case total
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = caseNumber * price;
+
+    //calculateTotal
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 //pone incerese decrese
 document.getElementById('phone-plus').addEventListener('click', function() {
-    updateProductNumber(
-        'phone', 1219, true);
+    updateProductNumber('phone', 1219, true);
 });
 document.getElementById('phone-minus').addEventListener('click', function() {
-    updateProductNumber(
-        'phone', 1219, false);
+    updateProductNumber('phone', 1219, false);
 });
 //handle case increase decrese event
 document.getElementById('case-plus').addEventListener('click', function() {
     /* const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
     caseInput.value = parseInt(caseNumber) + 1; */
-    updateProductNumber(
-        'case', 59, true);
+    updateProductNumber('case', 59, true);
 });
 document.getElementById('case-minus').addEventListener('click', function() {
     /*  const caseInput = document.getElementById('case-number');
      const caseNumber = caseInput.value; */
-    updateProductNumber(
-        'case', 59, false);
+    updateProductNumber('case', 59, false);
+
+
 
 });
