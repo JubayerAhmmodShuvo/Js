259c74c88f91856285f5d2498c99132cd82a2281
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-14 22:20:39 +0600

    practice

--- k.js
+++ k.js
@@ -7,15 +7,15 @@
  */
 // console.log(generatePin());
 // const r = (!isNaN("10X"));
-/* const product = 5;
+const product = 5;
 const price = "7";
 const subTotal = product * price;
 const tax = subTotal / 10;
-const total = subTotal + tax;
-console.log(total); */
-/* const task = 'Practice Makes a person perfect';
+console.log(total);
+const task = 'Practice Makes a person perfect';
 const index = task.toLowerCase().indexOf('M');
-console.log(index); */
+console.log(index); * /
+
 const str1 = 'Bangladesh is a beautiful country.';
 console.log(str1.endsWith('country'));
 const array = ["121", "12", "1", "112", "111"];
