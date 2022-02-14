8dfc48e3b4eadd84e382f65d586c2878d5d96e35
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-14 22:20:59 +0600

    practice

--- k.js
+++ k.js
@@ -1,10 +1,10 @@
 //  let count = 0;
 //  console.log(parseInt("count" + 1));
 //let r = Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9);
-/* function generatePin() {
+function generatePin() {
     return Math.floor(Math.random() * 90000) + 10000;
 }
- */
+
 // console.log(generatePin());
 // const r = (!isNaN("10X"));
 const product = 5;
