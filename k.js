8053a7c9dd67d446102aaab8ed0031db149d753b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-14 22:19:42 +0600

    practice

--- k.js
+++ k.js
@@ -1,15 +1,23 @@
 //  let count = 0;
 //  console.log(parseInt("count" + 1));
 //let r = Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9);
-function generatePin() {
+/* function generatePin() {
     return Math.floor(Math.random() * 90000) + 10000;
 }
-
+ */
 // console.log(generatePin());
 // const r = (!isNaN("10X"));
-const product = 5;
+/* const product = 5;
 const price = "7";
 const subTotal = product * price;
 const tax = subTotal / 10;
 const total = subTotal + tax;
-console.log(total);
+console.log(total); */
+/* const task = 'Practice Makes a person perfect';
+const index = task.toLowerCase().indexOf('M');
+console.log(index); */
+/* const str1 = 'Bangladesh is a beautiful country.';
+console.log(str1.endsWith('country')); */
+const array = ["121", "12", "1", "112", "111"];
+
+console.log(array.sort());
