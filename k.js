fade5b8a7b03b509fff5cbc3fc93fdbb27b2269e
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-14 22:20:04 +0600

    practice

--- k.js
+++ k.js
@@ -16,8 +16,8 @@ console.log(total); */
 /* const task = 'Practice Makes a person perfect';
 const index = task.toLowerCase().indexOf('M');
 console.log(index); */
-/* const str1 = 'Bangladesh is a beautiful country.';
-console.log(str1.endsWith('country')); */
+const str1 = 'Bangladesh is a beautiful country.';
+console.log(str1.endsWith('country'));
 const array = ["121", "12", "1", "112", "111"];
 
 console.log(array.sort());
